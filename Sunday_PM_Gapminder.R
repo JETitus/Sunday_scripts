8edbdabc8696916b5cf45f952e73e4a661fb4308
JETitus <james.mcquillan@mavs.uta.edu>
2015-04-19 14:35:53 -0500

    dplyr filteration pipeline added

--- Sunday_PM_Gapminder.R
+++ Sunday_PM_Gapminder.R
@@ -4,13 +4,25 @@
 # April 19th, 2015
 
 # Install dplyr and its depnedencies
-install.packages("dplyr", dependencies = TRUE)
+# install.packages("dplyr", dependencies = TRUE)
 
 # Load dplyr with its dependencies
 library("dplyr")
 
 # Read in data as tab-delimited
 gap.in = read.table("output/combined_gapMinder.tsv", sep = "\t", header=TRUE)
+
+# Parse data using dplyr
+gap.in %>%
+  filter(pop>15000000) %>%
+  select(country, year, pop) %>%
+  group_by(country, year) %>%
+  summarize(min=min(pop))
+
+# Challange
+# Calc the mean pop per continent per years for years prior to 1990
+
+
 
 for (row.number in 1:10){
   for (col.number in 1:5){
