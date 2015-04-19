9a16ddc28b04672779a89c1b606555c404b752d0
JETitus <james.mcquillan@mavs.uta.edu>
2015-04-19 13:40:43 -0500

    Fixed the Header=TRUE command

--- Sunday_PM_Gapminder.R
+++ Sunday_PM_Gapminder.R
@@ -3,4 +3,6 @@
 # jtmcquillan@uta.edu
 # April 19th, 2015
 
-gap.in = read.table("output/combined_gapMinder.tsv", sep = "\t", Header=TRUE)
+# Read in data as tab-delimited
+gap.in = read.table("output/combined_gapMinder.tsv", sep = "\t")
+
